f4462619fa7b4981fdc042cf3646b0c8d06ae7d0
aurelija <peleckaite.aurelija@gmail.com>
2018-03-01 11:25:57 +0200

    Initial commit

---
+++ app/src/main/java/ems/vtvpmc/android20180301/MainActivity.java
@@ -0,0 +1,17 @@
+package ems.vtvpmc.android20180301;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ZP17-2 on 2018.03.01.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
